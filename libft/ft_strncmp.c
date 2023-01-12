496e70f783f0f548541c4b67b9ae8fbf8f697ed7
Carlos Bravo Azuara <cbravo-a@c2r5s3.42madrid.com>
2023-01-12 17:21:09 +0100

    ee

---
+++ libft/ft_strncmp.c
@@ -0,0 +1,36 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strncmp.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: cbravo-a <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/01/12 14:17:40 by cbravo-a          #+#    #+#             */
+/*   Updated: 2023/01/12 16:44:12 by cbravo-a         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h> 
+
+int ft_strncmp(const char *s1, const char *s2, size_t n)
+{
+    size_t i;
+
+    i = 0;
+    while ((s1[i] != '\0' && s2[i] != '\0') && i < n)
+    {
+        if (s1[i] != s2[i])
+            return (s1[i] - s2[i]);
+        i++;
+    }
+    return (0);
+}
+
+/*int main(void)
+{
+    printf("MIA: %d\n", ft_strncmp("Buenas tardes", "Buenos dias", 9));
+    printf("OROGINAL: %d\n", strncmp("Buenas tardes", "Buenos dias", 9));
+    return (0);
+}*/
--- libft/ft_strnstr.c
+++ libft/ft_strnstr.c
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_strcmp.c                                        :+:      :+:    :+:   */
+/*   ft_strnstr.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: cbravo-a <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/01/12 14:17:40 by cbravo-a          #+#    #+#             */
-/*   Updated: 2023/01/12 14:23:21 by cbravo-a         ###   ########.fr       */
+/*   Created: 2023/01/12 16:08:04 by cbravo-a          #+#    #+#             */
+/*   Updated: 2023/01/12 17:13:02 by cbravo-a         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,20 +26,30 @@ size_t ft_strlen(const char *str)
     return (i);
 }
 
-/*int ft_strcmp(const char *s1, const char *s2)
+char *ft_strnstr(const char *s1, const char *s2, size_t len)
 {
-    int len1;
-    int len2;
-    
-    len1 = ft_strlen(s1);
-    len2 = ft_strlen(s2);
-    
-    
-}*/
+    size_t i;
+    char *prim;
+
+    i = 0;
+    prim = (char *)s1;
+    if (s2[i] == '\0')
+        return (prim);
+    while ((s1[i] != '\0' && s2[i] != '\0') && i < len)
+    {
+        if (s1[i] != s2[i])
+        {
+            if (s1[i] )
+            return (prim);    
+        }
+        i++;
+    }
+    return (NULL);
+}
 
 int main(void)
 {
-    //printf("MIA: %s\n", ft_strcmp("Buenas tardes", 101));
-    printf("OROGINAL: %d\n", strcmp("Buenas tardes", "Buenas nochesesesesese"));
+    printf("MIA: %s\n", ft_strnstr("Buenas tardes", "tardes", 9));
+    printf("OROGINAL: %s\n", strnstr("Buenas tardes", "tardes", 9));
     return (0);
 }
