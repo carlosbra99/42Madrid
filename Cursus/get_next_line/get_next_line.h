bd9f434536b7414265072f3e733806411d3ca15b
Carlos Bravo Azuara <carlos@cbaMac.local>
2023-03-01 17:28:59 +0100

    Miercoles1

---
+++ Cursus/get_next_line/get_next_line.h
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: cbravo-a <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/03/01 17:10:07 by cbravo-a          #+#    #+#             */
+/*   Updated: 2023/03/01 17:15:17 by cbravo-a         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# include <stdlib.h>
+# include <unistd.h>
+
+size_t	ft_strlen(const char *str);
+char    *get_next_line(int fd);
+
+#endif
