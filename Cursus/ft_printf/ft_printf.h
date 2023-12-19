8d21d102fabee25d6d4207c4584bf6fb36421bfe
Carlos Bravo Azuara <cbravo-a@c2r4s1.42madrid.com>
2023-12-19 20:33:11 +0100

    perfe

--- Cursus/ft_printf/ft_printf.h
+++ Cursus/ft_printf/ft_printf.h
@@ -6,7 +6,7 @@
 /*   By: cbravo-a <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/19 11:07:49 by cbravo-a          #+#    #+#             */
-/*   Updated: 2023/12/19 19:11:55 by cbravo-a         ###   ########.fr       */
+/*   Updated: 2023/12/19 19:39:55 by cbravo-a         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,12 +22,12 @@ int	ft_putnbr(int nb);
 int	ft_putunbr(unsigned int nb);
 int	ft_putchar(char c);
 int	ft_putstr(char *str);
-int ft_puthex_min(unsigned int nb);
-int ft_puthex_may(unsigned int nb);
-int ft_putptr(int *ptr);
+int	ft_puthex_min(unsigned int nb);
+int	ft_puthex_may(unsigned int nb);
+int	ft_putptr(int *ptr);
 int	ft_putptr_aux(unsigned long nb);
 
-int ft_printf_type(va_list args, const char type);
+int	ft_printf_type(va_list args, const char type);
 int	ft_printf(const char *str, ...);
 
 #endif
