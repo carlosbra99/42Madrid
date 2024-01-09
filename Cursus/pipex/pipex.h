cef94c129639bb0c6b275925a56ffa06e293f1da
carlosbra99 <122358855+carlosbra99@users.noreply.github.com>
2024-01-09 14:00:23 +0100

    jaja

--- Cursus/pipex/pipex.h
+++ Cursus/pipex/pipex.h
@@ -6,7 +6,7 @@
 /*   By: cbravo-a <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/03 13:06:16 by cbravo-a          #+#    #+#             */
-/*   Updated: 2024/01/09 13:10:23 by cbravo-a         ###   ########.fr       */
+/*   Updated: 2024/01/09 13:59:40 by cbravo-a         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,11 +20,11 @@
 # include <string.h>
 # include "./libft/libft.h"
 
-void    close_fds(int    pipefds[2]);
-int     ft_strichr(char *str, char c);
-char    *ft_strndup(char *str, unsigned int n);
-void    free_mat(char **mat);
-void    error_msg(void);
+void	close_fds(int pipefds[2]);
+int		ft_strichr(char *str, char c);
+char	*ft_strndup(char *str, unsigned int n);
+void	free_mat(char **mat);
+void	error_msg(void);
 
 char	*get_path(char *cmd, char **envp);
 char	*get_env_path(char **envp);
--- Cursus/pipex/src/utils.c
+++ Cursus/pipex/src/utils.c
@@ -6,55 +6,55 @@
 /*   By: cbravo-a <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/09 10:13:35 by cbravo-a          #+#    #+#             */
-/*   Updated: 2024/01/09 13:10:00 by cbravo-a         ###   ########.fr       */
+/*   Updated: 2024/01/09 13:58:20 by cbravo-a         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../pipex.h"
 
-void    close_fds(int    pipefds[2])
+void	close_fds(int pipefds[2])
 {
-    close(pipefds[0]);
-    close(pipefds[1]);
+	close(pipefds[0]);
+	close(pipefds[1]);
 }
 
-int     ft_strichr(char *str, char c)
+int	ft_strichr(char *str, char c)
 {
-    int     i;
+	int	i;
 
-    i = 0;
-    while (str[i] && str[i] != c)
-        i++;
-    if (str[i] == c)
-        return (i);
-    return (-1);
+	i = 0;
+	while (str[i] && str[i] != c)
+		i++;
+	if (str[i] == c)
+		return (i);
+	return (-1);
 }
 
-char    *ft_strndup(char *str, unsigned int n)
+char	*ft_strndup(char *str, unsigned int n)
 {
-    char            *s2;
-    unsigned int    i;
-    
-    i = 0;
-    s2 = malloc(sizeof(char) * (n + 1));
-    while (i < n)
-        s2[i++] = *str++;
-    s2[n] = 0;
-    return (s2);    
+	char			*s2;
+	unsigned int	i;
+
+	i = 0;
+	s2 = malloc(sizeof(char) * (n + 1));
+	while (i < n)
+		s2[i++] = *str++;
+	s2[n] = 0;
+	return (s2);
 }
 
-void    free_mat(char **mat)
+void	free_mat(char **mat)
 {
-    int     i;
+	int	i;
 
-    i = -1;
-    while (mat[++i])
-        free(mat[i]);
-    free(mat);
+	i = -1;
+	while (mat[++i])
+		free(mat[i]);
+	free(mat);
 }
 
-void    error_msg(void)
+void	error_msg(void)
 {
-    perror("Error");
-    exit(1);
+	perror("Error");
+	exit(1);
 }
