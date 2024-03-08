54877e055cac4d46cae06227ce406293e09db4eb
carlosbra99 <122358855+carlosbra99@users.noreply.github.com>
2024-03-08 18:05:07 +0100

    mid

--- Cursus/push_swap/push_swap.h
+++ Cursus/push_swap/push_swap.h
@@ -6,7 +6,25 @@
 /*   By: cbravo-a <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/22 10:15:49 by cbravo-a          #+#    #+#             */
-/*   Updated: 2024/02/22 10:16:02 by cbravo-a         ###   ########.fr       */
+/*   Updated: 2024/03/08 17:08:26 by cbravo-a         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef PUSH_SWAP_H
+# define PUSH_SWAP_H
+
+#include "libft/libft.h"
+#include <stdlib.h>
+#include <stdio.h>
+#include <unistd.h>
+
+typedef struct s_list
+{
+    int     content;
+    int     p_content;
+    int     index;
+    struct s_list   *next;
+    
+}                   t_list;
+
+#endif
