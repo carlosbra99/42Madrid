ce67d9830684af95d14e05dd30f149ee70db17d1
Carlos Bravo Azuara <cbravo-a@c3r1s3.42madrid.com>
2024-04-17 15:15:13 +0200

    psdef

--- Cursus/push_swap/srcs/split_argv.c
+++ Cursus/push_swap/srcs/split_argv.c
@@ -6,11 +6,11 @@
 /*   By: cbravo-a <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/17 13:53:44 by cbravo-a          #+#    #+#             */
-/*   Updated: 2024/04/17 14:42:07 by cbravo-a         ###   ########.fr       */
+/*   Updated: 2024/04/17 15:14:53 by cbravo-a         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "push_swap.h"
+#include "../include/push_swap.h"
 
 static size_t	ft_words(const char *s, char c)
 {
